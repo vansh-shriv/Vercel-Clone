5a6d9874ed89a5fb8e7db80f30ce3dd566b8aeba
vansh-shriv <vanshsvst@gmail.com>
2025-12-06 12:41:20 +0530

    vercel-clone

--- backend/vercel-deploy-service/src/aws.ts
+++ backend/vercel-deploy-service/src/aws.ts
@@ -2,10 +2,13 @@ import {S3} from "aws-sdk";
 import path from "path";
 import fs from "fs";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const s3 = new S3({
-    accessKeyId:"accessKeyId",
-    secretAccessKey:"secretAccessKey",
-    endpoint:"endpoint",
+    accessKeyId:"process.env.ACCESS_KEY_ID",
+    secretAccessKey:"process.env.SECRET_ACCESS_KEY",
+    endpoint:"process.env.ENDPOINT",
 })
 
 export function downloadS3Folder(prefix:string,){
--- backend/vercel-request-handler/src/index.ts
+++ backend/vercel-request-handler/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import {S3} from "aws-sdk";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const s3 = new S3({
-    accessKeyId:"accessKeyId",
-    secretAccessKey:"secretAccessKey",
-    endpoint:"endpoint",
+    accessKeyId:"process.env.ACCESS_KEY_ID",
+    secretAccessKey:"process.env.SECRET_ACCESS_KEY",
+    endpoint:"process.env.ENDPOINT",
 })
 
 const app = express();
